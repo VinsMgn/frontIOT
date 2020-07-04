6a8018d2f60b38940cfae926b1bd35c16339624e
vincent marignier <vincent.marignier@hotmail.fr>
2020-07-04 22:33:40 +0200

    push

---
+++ src/main/java/Data/Entreprise.java
@@ -0,0 +1,15 @@
+package Data;
+
+import java.util.UUID;
+
+public class Entreprise {
+
+    private UUID siret;
+    private String raisonSociale;
+    private Integer noRue;
+    private String rue;
+    private Integer codePostal;
+    private String city;
+    private Integer telephone;
+    private String contact;
+}
---
+++ src/main/java/Data/User.java
@@ -0,0 +1,11 @@
+package Data;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String userName;
+    private String password;
+    private String email;
+    private Integer wage;
+}
